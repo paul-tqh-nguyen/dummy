a2cff2caaef77d9ccfda96894c60d22fcc14417d
Nguyen <Paul.Nguyen@TRS.TEXAS.GOV>
2024-04-04 16:16:18 -0500

    Initial commit of main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+'''
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+1234
+'''
+
+def main() -> None:
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+
+if __name__ == '__main__':
+    main()
